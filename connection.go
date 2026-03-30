c402ee641eb637881be788b2378e9094d50feb7e
Toby <tobyxdd@gmail.com>
2026-03-29 22:11:53 -0700

    Revert "fix: set max datagram size in SetCongestionControl"
    This reverts commit 20e2c69139b79f6c5689d1bc7765a14417930c73.

--- connection.go
+++ connection.go
@@ -3160,7 +3160,6 @@ func estimateMaxPayloadSize(mtu protocol.ByteCount) protocol.ByteCount {
 
 // SetCongestionControl replace the current congestion control algorithm with a new one.
 func (c *Conn) SetCongestionControl(cc congestion.CongestionControl) {
-	cc.SetMaxDatagramSize(congestion.ByteCount(c.currentMTUEstimate.Load()))
 	c.sentPacketHandler.SetCongestionControl(cc)
 }
 
